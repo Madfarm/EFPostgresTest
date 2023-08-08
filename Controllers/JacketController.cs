3ea6ab0b50118207f07339d36c8f101396546c72
AnthonyScott <anthony.scott1005@gmail.com>
2023-08-07 22:55:25 -0400

    Add foreign-key to jacket model and update api endpoint

--- Controllers/JacketController.cs
+++ Controllers/JacketController.cs
@@ -6,8 +6,8 @@ using Microsoft.EntityFrameworkCore;
 namespace EFPostgresTest.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class JacketController : ControllerBase
+[Route("api/collections/[controller]")]
+public class JacketsController : ControllerBase
 {
     private readonly PsqlDbContext _context;
     public JacketController(PsqlDbContext context) 
--- Models/Jacket/Jacket.cs
+++ Models/Jacket/Jacket.cs
@@ -3,6 +3,7 @@ namespace EFPostTest.Models;
 public class Jacket
 {
     public int Id { get; set; }
+    public int? CollectionId { get; set; }
     public int Size { get; set; }
     public string? Material { get; set; }
     public int Price { get; set; }
